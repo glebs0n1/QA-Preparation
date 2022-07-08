d60f6c52ab7a1b07021bdb5addb58f60f4f8074d
Gleb Son <songleb81@gmail.com>
2022-07-08 16:54:46 +0300

    Update Automation.java

--- WebTest/src/main/java/Automation.java
+++ WebTest/src/main/java/Automation.java
@@ -5,10 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Automation {
     public static void main(String[] args) {
-System.setProperty("webdriver.chrome.driver","C:\\webdrivers\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
         
         WebDriver driver = new ChromeDriver();
-        driver.get(" https://istore.lt");
+        driver.get("https://istore.lt");
         driver.manage().window().maximize();
         WebElement searchString = driver.findElement(By.xpath("//input[@id = 'j-search']"));
         searchString.sendKeys("Iphone");
