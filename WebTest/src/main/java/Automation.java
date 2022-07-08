8b468afef22965522543e1bf61b40ead37d7f10c
Gleb Son <songleb81@gmail.com>
2022-07-08 16:53:26 +0300

    Update Automation.java

--- WebTest/src/main/java/Automation.java
+++ WebTest/src/main/java/Automation.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Automation {
     public static void main(String[] args) {
-
+System.setProperty("webdriver.chrome.driver","C:\\webdrivers\\chromedriver.exe");
+        
         WebDriver driver = new ChromeDriver();
         driver.get("https://istore.lt/?gclid=CjwKCAjwwo-WBhAMEiwAV4dybTu4hFMPevg4ap4F6bD51IqhYtN30GCNtbzN16JzKrWPIff-MIE1_BoCqFsQAvD_BwE");
         driver.manage().window().maximize();
